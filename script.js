558f2dc6762d378d5363ea2798ac9c42730489b1
Dylan Jones <dylanfzj@msn.com>
2023-06-05 18:38:43 +0100

    Skeleton to change

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-
-
+const getRandomElement = (array) => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  };
+  
+  const generateRandomMessage = () => {
+    const starters = [
+      "Believe in yourself",
+      "Stay positive",
+      "Dream big",
+      "Embrace challenges",
+      "Focus on your goals",
+    ];
+  
+    const middles = [
+      "and you will succeed",
+      "and great things will happen",
+      "and you'll overcome any obstacle",
+      "and you'll find happiness",
+      "and you'll make a difference",
+    ];
+  
+    const endings = [
+      "no matter what",
+      "one step at a time",
+      "in the face of adversity",
+      "with passion and determination",
+      "because you're capable",
+    ];
+  
+    const starter = getRandomElement(starters);
+    const middle = getRandomElement(middles);
+    const ending = getRandomElement(endings);
+  
+    const randomMessage = `${starter} ${middle} ${ending}`;
+    return randomMessage;
+  };
+  
+  console.log(generateRandomMessage());
 
 
 /*
